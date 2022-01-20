54a3373570ccaad7e7a53e5be4d15920bc6028e8
Matti Luukkainen <mluukkai@iki.fi>
2022-01-20 16:28:22 +0200

    part3-2

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -77,6 +87,12 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {

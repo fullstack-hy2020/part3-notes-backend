08312cc66fc722a036a604874eed46d989e69f67
Matti Luukkainen <mluukkai@iki.fi>
2022-01-24 10:10:09 +0200

    part4-3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./app') // varsinainen Express-sovellus
+const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')

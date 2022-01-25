9cda1c596820568418f967f0b117fb1e44eda8e2
Matti Luukkainen <mluukkai@iki.fi>
2022-01-25 10:37:52 +0200

    part4-8

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,13 +1,18 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
   response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id)
+
   if (note) {
     response.json(note.toJSON())
   } else {
@@ -17,14 +22,19 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,10 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({})
+    .populate('notes', { content: 1, date: 1 })
+
   response.json(users)
 })
 

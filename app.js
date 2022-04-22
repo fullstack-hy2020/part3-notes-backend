3b0d3eb8901e5f03ff013134293017dd477f7636
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-22 18:12:05 +0300

    reset router added

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const logger = require('./utils/logger')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -29,6 +30,11 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

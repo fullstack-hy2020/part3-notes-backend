bce893fb22993968727899dc6a7e78b0820744fb
Antonio-Kim <antoniok@sfu.ca>
2022-06-26 09:47:38 -0700

    add "_.id" instead of ".id"
    On noteSchema, the returnedObject.id is assigned as "returnedObject._id.toString()", but it should be assigned to "returnedObject._id" instead to remove id

--- models/note.js
+++ models/note.js
@@ -19,7 +19,7 @@ const noteSchema = new mongoose.Schema({
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
+    returnedObject_.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   }

09f2d5f149d1d5f61648132083a8910818b74f81
Henri Remonen <81435393+HRemonen@users.noreply.github.com>
2022-10-25 09:02:11 +0300

    Switch notes update function to use async await

--- controllers/notes.js
+++ controllers/notes.js
@@ -45,7 +45,7 @@ notesRouter.delete('/:id', async (request, response, next) => {
   }
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const note = {
@@ -53,11 +53,10 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  
+  response.json(updatedNote)
+
 })
 
 module.exports = notesRouter

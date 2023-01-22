7ab9beaaacd2fec248e77f7e276d9da38f556fe7
Matti Luukkainen <mluukkai@iki.fi>
2023-01-22 13:06:39 +0200

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const app = require('./app') // varsinainen Express-sovellus
-const http = require('http')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })

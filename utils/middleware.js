a39d46056c2d32cc4e7308a989a6784ee8a7cbb2
eemanioui <64502428+eemanioui@users.noreply.github.com>
2023-02-17 20:13:08 -0500

    source code to match lesson PR
    Updated the source code related to the lesson PR I just pushed a couple of minutes ago.

--- utils/middleware.js
+++ utils/middleware.js
@@ -20,7 +20,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(400).json({ error: 'token missing or invalid' })
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)

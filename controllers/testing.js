0c31ad0fbebc8e586efcfe7473ea090a3357f8aa
Vlad Kats <103802904+vkats90@users.noreply.github.com>
2023-05-23 18:38:59 -0600

    Changed the /api/testing/reset endpoint to accept a DELETE request
    Based on pull request #2912 on the content code

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
 
-router.post('/reset', async (request, response) => {
+router.delete('/reset', async (request, response) => {
   await Note.deleteMany({})
   await User.deleteMany({})
 

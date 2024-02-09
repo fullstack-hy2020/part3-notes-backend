9279593555b1f78f2256691db96883425568f5a8
Pablo Maffioli <92764736+pablo-maff@users.noreply.github.com>
2024-02-09 11:12:17 +0100

    part4-5 serve dist instead of build

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 

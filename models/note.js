13d04d32169fe6a6e31a014aaefb67217a185485
Matti Luukkainen <mluukkai@iki.fi>
2024-02-09 15:14:25 +0200

    part4-1

--- models/note.js
+++ models/note.js
@@ -1,23 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })

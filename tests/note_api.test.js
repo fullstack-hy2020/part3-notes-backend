b57a461dd5425b36fd50868c4a6a4c791ff0b01f
Matti Luukkainen <mluukkai@iki.fi>
2024-02-12 15:08:51 +0200

    part4-3

---
+++ tests/note_api.test.js
@@ -0,0 +1,53 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('HTML is easy'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === 'test' 
+const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 

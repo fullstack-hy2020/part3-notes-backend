fa9db438cb17e0c2731eea7dbcd5769654e94145
Matti Luukkainen <mluukkai@iki.fi>
2024-02-13 13:32:41 +0200

    part 4-8

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -21,12 +23,18 @@ notesRouter.get('/:id', (request, response, next) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 

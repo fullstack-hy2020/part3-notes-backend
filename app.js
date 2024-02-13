a1e0cfadbe4a2d115cf42dca260487865cff32b6
Matti Luukkainen <mluukkai@iki.fi>
2024-02-13 19:07:08 +0200

    part5-1

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
---
+++ controllers/testing.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Note = require('../models/note')
+const User = require('../models/user')
+
+router.post('/reset', async (request, response) => {
+  await Note.deleteMany({})
+  await User.deleteMany({})
+
+  response.status(204).end()
+})
+
+module.exports = router

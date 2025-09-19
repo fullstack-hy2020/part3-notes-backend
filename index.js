84bfd3271bfb10e240ba1f4fe7ce21edc5d6196f
Jairo Celada <jairocelada@gmail.com>
2025-09-19 16:19:55 -0400

    Modificacion del puerto

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
